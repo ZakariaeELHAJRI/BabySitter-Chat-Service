c9e8c78ac43682dde069746ff3b44c58aa7b67e6
Zakariae elhajri <zakriaelhajri@gmail.com>
2024-01-07 19:23:37 +0100

    include routers message and conversation

--- api/__init__.py
+++ api/__init__.py
@@ -1,9 +1,11 @@
 from typing import Union
 
 from fastapi import FastAPI
+from api.routers import message , conversation
 
 app = FastAPI()
 
+#uvicorn main:app --reload     
 
 @app.get("/")
 def read_root():
@@ -13,3 +15,6 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+app.include_router(message.router, prefix="/api/chat", tags=["message"])
+app.include_router(conversation.router, prefix="/api/chat", tags=["conversation"])
